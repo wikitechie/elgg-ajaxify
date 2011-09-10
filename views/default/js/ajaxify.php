29bb864264b5b0e5c847cf3741348a65242fb5e0
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2011-09-10 22:26:11 +0300

    Overrided elgg.action so it doesn't make any system message

--- views/default/js/ajaxify.php
+++ views/default/js/ajaxify.php
@@ -116,4 +116,80 @@ elgg.ajaxify.getViewFromURL = function(value) {
 	return viewname;
 };
 
+/**
+ * Perform an action via ajax
+ *
+ * @example Usage 1:
+ * At its simplest, only the action name is required (and anything more than the
+ * action name will be invalid).
+ * <pre>
+ * elgg.action('name/of/action');
+ * </pre>
+ *
+ * The action can be relative to the current site ('name/of/action') or
+ * the full URL of the action ('http://elgg.org/action/name/of/action').
+ *
+ * @example Usage 2:
+ * If you want to pass some data along with it, use the second parameter
+ * <pre>
+ * elgg.action('friend/add', { friend: some_guid });
+ * </pre>
+ *
+ * @example Usage 3:
+ * Of course, you will have no control over what happens when the request
+ * completes if you do it like that, so there's also the most verbose method
+ * <pre>
+ * elgg.action('friend/add', {
+ *     data: {
+ *         friend: some_guid
+ *     },
+ *     success: function(json) {
+ *         //do something
+ *     },
+ * }
+ * </pre>
+ * You can pass any of your favorite $.ajax arguments into this second parameter.
+ *
+ * @note If you intend to use the second field in the "verbose" way, you must
+ * specify a callback method or the data parameter.  If you do not, elgg.action
+ * will think you mean to send the second parameter as data.
+ *
+ * @note You do not have to add security tokens to this request.  Elgg does that
+ * for you automatically.
+ *
+ * @see jQuery.ajax
+ *
+ * @param {String} action The action to call.
+ * @param {Object} options
+ * @return {XMLHttpRequest}
+ */
+elgg.action = function(action, options) {
+	elgg.assertTypeOf('string', action);
+
+	// support shortcut and full URLs
+	// this will mangle URLs that aren't elgg actions.
+	// Use post, get, or ajax for those.
+	if (action.indexOf('action/') < 0) {
+		action = 'action/' + action;
+	}
+
+	options = elgg.ajax.handleOptions(action, options);
+
+	options.data = elgg.security.addToken(options.data);
+	options.dataType = 'json';
+
+	//Always display system messages after actions
+	var custom_success = options.success || elgg.nullFunction;
+	options.success = function(json, two, three, four) {
+		if (json && json.system_messages) {
+			//elgg.register_error(json.system_messages.error);
+			//elgg.system_message(json.system_messages.success);
+		}
+
+		custom_success(json, two, three, four);
+	};
+
+	return elgg.post(options);
+};
+
 elgg.register_hook_handler('init', 'system', elgg.ajaxify.init);
